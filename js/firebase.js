428ec5f41b202a8a0a5c665532212d7284bc32c8
unknown <myemail~>
2023-06-07 17:58:26 -0300

    alters

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3kd_Xxy1oEfhMFvTfbeAVYAel0_ChVNk",
+  authDomain: "arara-senhas-2bc80.firebaseapp.com",
+  projectId: "arara-senhas-2bc80",
+  storageBucket: "arara-senhas-2bc80.appspot.com",
+  messagingSenderId: "429708375561",
+  appId: "1:429708375561:web:2292a1a9366aee0efaaad8",
+  measurementId: "G-J1QEDE9GCJ"
+};
+
+// Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
